c4524b3f585b22d270aa9905a01fd342d463f728
QiPengBian <bqp18975203048@gmail.com>
2020-03-28 16:46:43 +0800

    aop切面

---
+++ spring-boot-02-aop/src/main/java/com/study/SpringBoot02AopApplication.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: QiPeng
+ * @version: 1.0.0
+ * @description: TODO
+ * @createTime: 2020-03-28 16:08:00
+ */
+@SpringBootApplication
+public class SpringBoot02AopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02AopApplication.class, args);
+    }
+}
